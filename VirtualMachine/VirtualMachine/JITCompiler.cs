6f446a359801e52624a5befe9a257fe0ebeb8136
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-12-03 16:46:17 +0000

    add efficient instancing of instruction without operands

--- VirtualMachine/VirtualMachine/JITCompiler.cs
+++ VirtualMachine/VirtualMachine/JITCompiler.cs
@@ -4,6 +4,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Reflection;
     #endregion
     /// <summary>
@@ -17,7 +18,7 @@
 
         #region Fields
         private static Type[] SVMtypes = GetSVMTypes();
-        private static List<IInstruction> instantiatedTypes;
+        private static List<IInstruction> instantiatedTypes = new List<IInstruction>();
         private static Type type;
         #endregion
 
@@ -58,13 +59,23 @@
                 { 
                     try
                     {
-                        Type.GetType(SVMtypes[i].AssemblyQualifiedName).GetInterface("IInstruction");
-                        // If instruction instance exists...
-                        instruction = (IInstruction)Activator.CreateInstance(Type.GetType(SVMtypes[i].AssemblyQualifiedName));
+                        Type.GetType(SVMtypes[i].AssemblyQualifiedName).GetInterface("IInstruction"); // If this fails -> exception.
+                        // Check for existing instance -> exists -> reuse.
+                        foreach (IInstruction instance in instantiatedTypes)
+                        {
+                            if (instance.ToString().Split(" ")[0] == Type.GetType(SVMtypes[i].AssemblyQualifiedName).Name)
+                            {
+                                return instance;
+                            }
+                        }
+                        // Instance not found -> add new.
+                        instantiatedTypes.Add((IInstruction)Activator.CreateInstance(Type.GetType(SVMtypes[i].AssemblyQualifiedName)));
+                        return instantiatedTypes.Last();
                     }
                     catch
                     {
-                        Console.WriteLine("Invalid instruction...... exception message.....");
+                        // throw SVMexception.
+                        Console.WriteLine("Invalid instruction interface...... exception message.....");
                     }
                     return instruction;
                 }
@@ -79,16 +90,64 @@
 
             #region TASK 1 - TO BE IMPLEMENTED BY THE STUDENT
             for (int i = 0; i < SVMtypes.Length; i++)
-            {
-                //LoadString    
+            { 
                 if (opcode.Equals(SVMtypes[i].Name.ToString(), StringComparison.InvariantCultureIgnoreCase))
                 {
+                    try
+                    {
+                        Type.GetType(SVMtypes[i].AssemblyQualifiedName).GetInterface("IInstruction"); // If this fails -> exception.
+                        #region JON PLS EXPLAIN HOW TO SET THE OPERANDS OF AN INSTANCED TYPE I CANNOT.
+                        //foreach (IInstruction instance in instantiatedTypes)
+                        //{
+                        //    if (instance.ToString().Split(" ")[0] == "*") // Change to any basic instruction. Set to * for task 5.
+                        //    {
+                        //        if (instance.ToString().Split(" ")[1] == Type.GetType(SVMtypes[i].AssemblyQualifiedName).Name)
+                        //        {
+                        //            PropertyInfo property = Type.GetType(SVMtypes[i].AssemblyQualifiedName).GetProperty("Operands");
+                        //            property.SetValue(instance, operands);
+                        //            return instance;
+                        //        }
+                        //    }
+                        //    else
+                        //    {
+                        //        if (instance.ToString().Split(" ")[0] == Type.GetType(SVMtypes[i].AssemblyQualifiedName).Name)
+                        //        {
+                        //            PropertyInfo property = instance.GetType().GetProperty("Operands");
+                        //           /* IMPOSSIBLE JON PLS
+                        //            property.GetType().GetProperty("Item").SetValue(instance, operands, new object[] { 0 });
+                        //                //.GetValue(instance, null);
+                        //            //var why = test.GetType().GetProperty("Item");
+                        //                //.SetValue(instance, operands, new object[] { (int) 0 });
+                        //            //property.SetValue(instance, operands);
+                                       
+                        //            //.SetValue(x, operands, new object[] { (int)0 });
+                        //            //property.SetValue();
+                        //            */
+                        //            return instance;
+                        //        }
+                        //    }
+                        //    break;
+                            
+                        //}
+                        //// Instance not found -> add new.
+                        //var newInstance = (IInstruction)Activator.CreateInstance(Type.GetType(SVMtypes[i].AssemblyQualifiedName));
+                        //PropertyInfo newProperty = Type.GetType(SVMtypes[i].AssemblyQualifiedName).GetProperty("Operands");
+                        //newProperty.SetValue(newInstance, operands);
+                        //instantiatedTypes.Add(newInstance);
+                        //return instantiatedTypes.Last();
+                        #endregion
+                    }
+                    catch
+                    {
+                        // throw SVMexception.
+                        Console.WriteLine("Invalid instruction interface...... exception message.....");
+                    }
+
                     type = Type.GetType(SVMtypes[i].AssemblyQualifiedName);
- 
                     Object obj = Activator.CreateInstance(type);
                     PropertyInfo property = type.GetProperty("Operands");
                     property.SetValue(obj, operands);
-                    return (IInstructionWithOperand) obj;
+                    return (IInstructionWithOperand)obj;
                 }
             }
             return instruction;
