f8b1d677192d82003ff350e82dee4dba53e5e4b5
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-12-04 11:10:47 +0000

    add debug classes

---
+++ VirtualMachine/VirtualMachine/Debug/DebugFrame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVM.VirtualMachine.Debug
+{
+    class DebugFrame : IDebugFrame
+    {
+        public IInstruction _currentInstruction = null;
+        public List<IInstruction> _codeFrame = null;
+        public DebugFrame(IInstruction currentInstruction, List<IInstruction> codeFrame)
+        {
+            _currentInstruction = currentInstruction;
+            _codeFrame = codeFrame;
+        }
+        public IInstruction CurrentInstruction
+        {
+            get => _currentInstruction;
+            set => _currentInstruction = value;
+        }
+
+        public List<IInstruction> CodeFrame
+        {
+            get => _codeFrame;
+            set => _codeFrame = value;
+        }
+    }
+}
