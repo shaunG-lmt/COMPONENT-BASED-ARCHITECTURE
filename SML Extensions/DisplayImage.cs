eaf309e8d8f8363288c849ad52b596d7c3d322e7
shaunG-lmt <53610279+shaunG-lmt@users.noreply.github.com>
2020-12-09 12:31:01 +0000

    add imageloading/display instructions

---
+++ SML Extensions/DisplayImage.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SVM.VirtualMachine;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+
+namespace SML_Extensions
+{
+    class DisplayImage : BaseInstructionWithOperand
+    {
+        #region Constants
+        static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPE", ".BMP", ".GIF", ".PNG" };
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public methods
+        #endregion
+
+        #region Non-public methods
+        #endregion
+
+        #region System.Object overrides
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object">Object</see> is equal to the current <see cref="System.Object">Object</see>.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object">Object</see> to compare with the current <see cref="System.Object">Object</see>.</param>
+        /// <returns><b>true</b> if the specified <see cref="System.Object">Object</see> is equal to the current <see cref="System.Object">Object</see>; otherwise, <b>false</b>.</returns>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for this type.
+        /// </summary>
+        /// <returns>A hash code for the current <see cref="System.Object">Object</see>.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String">String</see> that represents the current <see cref="System.Object">Object</see>.
+        /// </summary>
+        /// <returns>A <see cref="System.String">String</see> that represents the current <see cref="System.Object">Object</see>.</returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        #endregion
+
+        #region IInstruction Members
+        public override void Run()
+        {
+            var path = VirtualMachine.Stack.Peek();
+            if (File.Exists(path.ToString()))
+            {
+                if(ImageExtensions.Contains(Path.GetExtension(path.ToString()).ToUpperInvariant()))
+                {
+                    var f = new Form();
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Image = Image.FromFile(path.ToString());
+                    pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+                    f.Controls.Add(pictureBox);
+                    //f.Show();
+                    Thread thread = new Thread(() => Application.Run(f));
+
+                    thread.Start();
+                    
+                }
+                else
+                {
+                    throw new SvmRuntimeException("File path on top of stack is not valid image. File path: " + path);
+                }
+                
+            }
+            else
+            {
+                throw new SvmRuntimeException("Value on top of stack point to file does not exist. Value at top of stack currently: " + path);
+            }
+
+            
+        }
+        #endregion
+    }
+}
---
+++ SML Extensions/LoadImage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SVM.VirtualMachine;
+
+namespace SML_Extensions
+{
+    class LoadImage : BaseInstructionWithOperand
+    {
+        #region Constants
+        static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPE", ".BMP", ".GIF", ".PNG" };
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public methods
+        #endregion
+
+        #region Non-public methods
+        #endregion
+
+        #region System.Object overrides
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object">Object</see> is equal to the current <see cref="System.Object">Object</see>.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object">Object</see> to compare with the current <see cref="System.Object">Object</see>.</param>
+        /// <returns><b>true</b> if the specified <see cref="System.Object">Object</see> is equal to the current <see cref="System.Object">Object</see>; otherwise, <b>false</b>.</returns>
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        /// <summary>
+        /// Serves as a hash function for this type.
+        /// </summary>
+        /// <returns>A hash code for the current <see cref="System.Object">Object</see>.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String">String</see> that represents the current <see cref="System.Object">Object</see>.
+        /// </summary>
+        /// <returns>A <see cref="System.String">String</see> that represents the current <see cref="System.Object">Object</see>.</returns>
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        #endregion
+
+        #region IInstruction Members
+        public override void Run()
+        {
+            if (Operands[0].GetType() != typeof(string))
+            {
+                throw new SvmRuntimeException(String.Format(BaseInstruction.OperandOfWrongTypeMessage,
+                                                this.ToString(), VirtualMachine.ProgramCounter));
+            }
+            if (File.Exists(Operands[0]))
+            {
+                if(ImageExtensions.Contains(Path.GetExtension(Operands[0]).ToUpperInvariant()))
+                {
+                    VirtualMachine.Stack.Push(Operands[0]);
+                }
+                else
+                {
+                    throw new SvmRuntimeException("File is not valid image.");
+                }
+                
+            }
+            else
+            {
+                throw new SvmRuntimeException("File does not exist.");
+            }
+
+            
+        }
+        #endregion
+    }
+}
